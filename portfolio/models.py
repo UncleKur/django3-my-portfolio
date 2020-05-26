60a8d6b5c60a6c3a9e1768cc5aa267fb983f3e7f
Mikhail Kurochkin <eaq1993@rambler.ru>
2020-05-26 21:27:43 +0400

    Created new model for Portfolio app called 'Ability', added two lines in INSTALLED_APP(Blog and Portfolio)

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Ability(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
